192e3a1618656eac377d4d384adcd0c8ad5e4630
Varun Sharma <varun@arintech.in>
2024-12-04 16:06:59 +0530

    updated

--- networking.tf
+++ networking.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "main-vpc" {
-cidr_block = "10.22.0.0/16"
+cidr_block = var.vpc_cidr_block
 enable_dns_hostnames = "true"
 tags = {
-   Name = "student.1-vpc"
+   Name = var.vpc_name
     }
 }
 
@@ -11,14 +11,14 @@ resource "aws_subnet" "subnet-1" {
   cidr_block = var.subnet-1_cidr_block
   map_public_ip_on_launch = "true"
   tags = {
-    Name = "student.1-subnet-1"
+    Name = var.subnet-1_name
   }
 }
 
 resource "aws_internet_gateway" "igw" {
  vpc_id = aws_vpc.main-vpc.id
  tags = {
-   Name = "student.1-igw"
+   Name = var.igw_name
  }
 }
 
@@ -30,7 +30,7 @@ resource "aws_route_table" "my-route-table" {
        gateway_id = aws_internet_gateway.igw.id
  }
 tags = {
-     Name = "student.1-route-table"
+     Name = var.route_table_name
  } 
 }
 
