732cd46de6ebf644ae60391a6caf4a07d3876683
Varun Sharma <varun@arintech.in>
2024-12-04 12:46:24 +0530

    subnet

---
+++ output.tf
@@ -0,0 +1,18 @@
+output "my_vpc" {
+ value = aws_vpc.main-vpc.id
+} 
+
+output "my_subnet" {
+ value = aws_subnet.subnet-1.id
+}
+
+output "my_igw" {
+ value = aws_internet_gateway.igw.id
+}
+
+output "my_route_table" {
+  value       = aws_route_table.my-route-table.id
+}
+output "my_route_table_association" {
+  value       = aws_route_table_association.my-route-association.id
+}
